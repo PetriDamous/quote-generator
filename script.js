1f5c86a836bd79460a3df12fdb05b5cf6a22265e
dwilliams64 <dwilliams_64@yahoo.com>
2020-09-13 13:46:58 -0500

    Added twitter button to post quotes on twitter

--- script.js
+++ script.js
@@ -17,31 +17,16 @@ const longQoute = () => {
     }
 }
 
+const tweetQuote = () => {
+    const quote = quoteTxt.textContent;    
+    const author = authorTxt.textContent;
+    const tweeterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(tweeterUrl, "_blank");
+}
+
 // Fetch Quote from API
 async function getQuote () {    
     const apiUrl = "https://type.fit/api/quotes";
-
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-    
-    const quote = data[randomNum(0, data.length - 1)];
-
-    // let long = 0;
-
-    // data.forEach((quote, idx) => {
-        
-    //     // if(long < quote.text.length) long = quote.text.length
-        
-    //     // long = long < quote.text.length ? quote.text.length : long;
-        
-    //     // console.log(long);   
-        
-    //     // if (quote.text.length === long) console.log(quote.text)
-
-    //     if (quote.text.length === 130) console.log(quote.text)
-        
-    // })
-
 
     try {
 
@@ -52,30 +37,23 @@ async function getQuote () {
 
         let { text, author } = quote;
 
-        author = author === null ? "unknown" : author;
-
-        // quoteTxt.textContent = "Translation is the paradigm, the exemplar of all writing. It is translation that demonstrates most vividly the yearning for transformation that underlies every act involving speech, that supremely human gift.";
-
-        // quoteTxt.textContent = "Intuition is the very force or activity of the soul in its experience through whatever has been the experience of the soul itself. fgkjsjgkjfsjg fdlksaj ldjflajlk dk dk lkdsjfa dlkfjaj fdlksflafjlda dlkajflka;faj aldsf;"
-
-        console.log(quoteTxt.textContent.length)
-
         longQoute();
 
         quoteTxt.textContent = text;
-        authorTxt.textContent = author;
+        authorTxt.textContent = author === null ? "unknown" : author;
 
         
 
     } catch (error) {
         getQuote();        
-        console.log("Nigga wtf is going on?", error);
     }
 }
 
 window.addEventListener("resize", longQoute);
 
 newQuoteBtn.addEventListener("click", getQuote);
+
+twitterBtn.addEventListener("click", tweetQuote);
 
 // On Load
 getQuote();
